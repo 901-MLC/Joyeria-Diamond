c36c756b6651ca0ebe9fefb4675c67bcf218f915
Lariosk <105257073+Lariosk@users.noreply.github.com>
2026-09-23 14:02:54 -0600

    Create database and CLIENTES table in SQL

--- CONFIGURACION/BD-JOYERIA.sql
+++ CONFIGURACION/BD-JOYERIA.sql
@@ -1,1 +1,11 @@
-CREATE BD JOYERIA
+create DATABASE BD_JOYERIA;
+use BD_JOYERIA;
+table CLIENTES (
+    id_cliente INT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(100) NOT NULL,
+    apellido VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    telefono VARCHAR(15),
+    direccion VARCHAR(255)
+);
+
